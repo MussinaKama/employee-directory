feee52775002c3f989f208c8a4dfce7f3fa0bb74
Kamshat Mussina <mussinak777@gmail.com>
2020-02-16 11:34:23 -0700

    Updates

---
+++ precache-manifest.98ce7709b7ab02d34eac553154e2de48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f566bd6fc7b6c49f53883838fe44eefd",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "63cc3e7ccfc19698f604",
+    "url": "/employee-directory/static/css/2.3978bfa6.chunk.css"
+  },
+  {
+    "revision": "7c894598ba19bced54d8",
+    "url": "/employee-directory/static/css/main.8ee7a296.chunk.css"
+  },
+  {
+    "revision": "63cc3e7ccfc19698f604",
+    "url": "/employee-directory/static/js/2.747e7271.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/employee-directory/static/js/2.747e7271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c894598ba19bced54d8",
+    "url": "/employee-directory/static/js/main.cb348469.chunk.js"
+  },
+  {
+    "revision": "100984a94e34414a2f61",
+    "url": "/employee-directory/static/js/runtime-main.6cd53313.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/employee-directory/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/employee-directory/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/employee-directory/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/employee-directory/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/employee-directory/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
